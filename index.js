575990fd54de47ea93701449e886a8d89e7d8177
abdullah <anasircft@gmail.com>
2025-02-12 19:46:12 +0500

    Add User Create Api and Login Api

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ require("./src/config/config");
 const { ApolloServer } = require("apollo-server-express");
 const typeDefs = require("./src/graphql/user/typeDefs");
 const resolvers = require("./src/graphql/user/resolvers");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
+dotenv.config();
 const app = express();
+
+app.use(cors());
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req, res }) => ({ req, res }),
 });
 
 async function startServer() {
   await server.start();
-  server.applyMiddleware({ app });
+  server.applyMiddleware({ app, cors: false });
 
   const port = process.env.PORT;
   const ip = process.env.IP;
